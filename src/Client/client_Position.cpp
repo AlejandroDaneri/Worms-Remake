1f6ac8e7119af188e1af43a1d621ec22bf558705
aguszorza <aguszorza@gmail.com>
2018-05-09 21:08:50 -0300

    Cambio coordenadas

--- src/Client/client_Position.cpp
+++ src/Client/client_Position.cpp
@@ -5,6 +5,17 @@ Position::Position(int x, int y): x(x), y(y){}
 		
 Position::~Position(){}
 
+Position::Position(const Position& other) {
+	this->x = other.x;
+	this->y = other.y;
+}
+
+Position& Position::operator=(const Position& other) {
+	this->x = other.x;
+	this->y = other.y;
+	return *this;
+}
+
 bool Position::operator==(const Position& other){
 	return this->x == other.x && this->y == other.y;
 }
