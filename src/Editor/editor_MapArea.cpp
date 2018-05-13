dec72ed5fa25edba2e6f591e054dda40324f0d8b
Alejandro Daneri <ale_dan_94@hotmail.com>
2018-05-13 18:34:48 +0000

    soporte para multiples objetos

--- src/Editor/editor_MapArea.cpp
+++ src/Editor/editor_MapArea.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <gdkmm.h>
 #include "editor_MapArea.h"
+#include "editor_Item.h"
 
 MapArea::MapArea(ToolBox *toolBox) {
     add_events(Gdk::BUTTON_PRESS_MASK);
@@ -12,21 +13,20 @@ MapArea::MapArea(ToolBox *toolBox) {
 }
 
 bool MapArea::on_draw(const ::Cairo::RefPtr<::Cairo::Context> &cr) {
-    cr->set_source_rgb(1, 1, 0); //para ver donde esta
-    cr->rectangle(0, 0, get_width(), get_height());
-    cr->fill();
-    if (ok) Gdk::Cairo::set_source_pixbuf
-                (cr, new_image, x-new_image->get_width()/2,
-                 y-new_image->get_height()/2);
-    cr->paint();
+    for(Item item:items ){
+        Gdk::Cairo::set_source_pixbuf
+                (cr, item.getImage(), item.getX(),item.getY());
+        cr->paint();
+    }
     return true;
 }
 
 bool MapArea::on_button_press_event(GdkEventButton *button_event) {
-    try
-    {
+    try {
         const std::string &path = toolBox->getActualItemImagePath();
-        new_image = Gdk::Pixbuf::create_from_file(path);
+        Glib::RefPtr<Gdk::Pixbuf> new_image = Gdk::Pixbuf::create_from_file(path);
+        Item new_item(std::move(new_image),button_event->x,button_event->y);
+        items.push_back(std::move(new_item));
     }
     catch(const Gio::ResourceError& ex)
     {
@@ -36,10 +36,6 @@ bool MapArea::on_button_press_event(GdkEventButton *button_event) {
     {
         std::cerr << "PixbufError: " << ex.what() << std::endl;
     }
-    
-    ok =true;
-    this->x=button_event->x;
-    this->y=button_event->y;
     queue_draw();
     return true;
 }
