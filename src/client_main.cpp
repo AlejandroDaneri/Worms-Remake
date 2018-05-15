8545b18f694da08c97314743a373fe47418d5173
aguszorza <aguszorza@gmail.com>
2018-05-15 01:13:06 -0300

    Atrapo la excepcion de socket en el main

--- src/client_main.cpp
+++ src/client_main.cpp
@@ -9,24 +9,28 @@
 #include "client_Player.h"
 #include "DataReceiver.h"
 
+#include <iostream>
+
 int main(int argc, char* argv[]){
 
 	auto app = Gtk::Application::create(argc, argv);
     Gtk::Window window;
     window.resize(1280, 720);
+	try {
+    	Socket socket(Socket::Client("127.0.0.1", "7777"));
+    
+		ClientProtocol protocol(std::move(socket));
 
-    Socket socket(Socket::Client("127.0.0.1", "7777"));
-    ClientProtocol protocol(std::move(socket));
+		Player player(protocol);
 
-    Player player(protocol);
+		window.add(player.getWindow());
+		window.show_all();
 
-    window.add(player.getWindow());
-    window.show_all();
-
-
-    try{
-        app->run(window);
-    } catch (const std::exception& e){}
+		app->run(window);
+    } catch (const std::exception& e){
+    	std::cout << e.what() << std::endl;
+    	return 1;
+    }
 
 	return 0;
 }
