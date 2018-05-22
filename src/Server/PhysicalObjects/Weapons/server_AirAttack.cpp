daab452ed76c05b564066e8bb87ca1bb761a9647
feldmatias <feldmatias@gmail.com>
2018-05-21 22:17:38 -0300

    mejoro misiles aereos

--- src/Server/PhysicalObjects/Weapons/server_AirAttack.cpp
+++ src/Server/PhysicalObjects/Weapons/server_AirAttack.cpp
@@ -2,7 +2,7 @@
 #include "server_WeaponFactory.h"
 #include "Worm.h"
 
-#define SEPARATION 1.0
+#define SEPARATION (weapon_size * 2)
 
 AirAttack::AirAttack(World& world, GameParameters& parameters):
 	Weapon(world, parameters, 0){}
@@ -17,8 +17,8 @@ void AirAttack::shoot(char dir, int angle, int power, int time){}
 
 void AirAttack::shoot(Worm& shooter, b2Vec2 pos){
 	int missiles = this->parameters.getWeaponFragments(AIR_ATTACK_NAME);
-	int pos_x = pos.x - missiles * SEPARATION / 2;
-	int pos_y = this->parameters.getMaxHeight();
+	float pos_x = pos.x - missiles * SEPARATION / 2;
+	float pos_y = this->parameters.getMaxHeight();
 	WeaponFactory factory(this->world, this->parameters);
 	for (int i = 0; i < missiles; i++, pos_x += SEPARATION){
 		physical_object_ptr missile = factory.getWeapon(AIR_ATTACK_MISSILE_NAME);
