06b39b1ce3763ccf3a0fe565385da48f219344c7
Alejandro Daneri <ale_dan_94@hotmail.com>
2018-05-23 19:49:53 -0300

    guardar ahora usa los defines

--- src/Editor/FileWriter.cpp
+++ src/Editor/FileWriter.cpp
@@ -1,6 +1,8 @@
 
 #include <yaml.h>
 #include <iostream>
+#include <WeaponNames.h>
+#include <ConfigFields.h>
 #include "FileWriter.h"
 #include "MapObject.h"
 
@@ -15,40 +17,40 @@ void FileWriter::save(const std::vector<int>& weapons,
 
     out << YAML::BeginMap;
 
-    out << YAML::Key << "worms_life";
+    out << YAML::Key << WORMS_LIFE;
     out << YAML::Value << 200;
 
     //armas
-    out << YAML::Key << "weapon_ammo";
+    out << YAML::Key << WEAPON_AMMO;
     out << YAML::Value << YAML::BeginMap;
-    out << YAML::Key << "Bazooka";
+    out << YAML::Key << BAZOOKA_NAME;
     out << YAML::Value << weapons[0];
-    out << YAML::Key << "Mortar";
+    out << YAML::Key << MORTAR_NAME;
     out << YAML::Value << weapons[1];
-    out << YAML::Key << "GreenGrenade";
+    out << YAML::Key << GREEN_GRENADE_NAME;
     out << YAML::Value << weapons[2];
-    out << YAML::Key << "RedGrenade";
+    out << YAML::Key << RED_GRENADE_NAME;
     out << YAML::Value << weapons[3];
-    out << YAML::Key << "Banana";
+    out << YAML::Key << BANANA_NAME;
     out << YAML::Value << weapons[4];
-    out << YAML::Key << "HolyGrenade";
+    out << YAML::Key << HOLY_GRENADE_NAME;
     out << YAML::Value << weapons[9];
-    out << YAML::Key << "Dynamite";
+    out << YAML::Key << DYNAMITE_NAME;
     out << YAML::Value << weapons[8];
-    out << YAML::Key << "Bat";
+    out << YAML::Key << BAT_NAME;
     out << YAML::Value << weapons[6];
-    out << YAML::Key << "AirAttack";
+    out << YAML::Key << AIR_ATTACK_NAME;
     out << YAML::Value << weapons[5];
-    out << YAML::Key << "Teleportation";
+    out << YAML::Key << TELEPORT_NAME;
     out << YAML::Value << weapons[7];
 
     out << YAML::EndMap;
 
     //posicion de gusanos
-    out << YAML::Key << "worms";
+    out << YAML::Key << WORMS_DATA;
     out << worms;
 
-    out << YAML::Key << "girders";
+    out << YAML::Key << GIRDERS_DATA;
     out << girders;
 
 
