03834a2cbe88dc9f238f4e23bf9e4a47ec08ada9
feldmatias <feldmatias@gmail.com>
2018-05-27 17:50:09 -0300

    empiezo con menu cliente

--- src/Client/Menu/ServerMenu.cpp
+++ src/Client/Menu/ServerMenu.cpp
@@ -1,16 +1,46 @@
 #include "ServerMenu.h"
+#include "Path.h"
+#include <gtkmm/builder.h>
 
 ServerMenu::ServerMenu(Gtk::Window& window): window(window){
-	this->menu.pack_start(this->error);
-	this->menu.pack_start(this->host);
-	this->menu.pack_start(this->service);
-	this->menu.pack_start(this->connect);
-	this->window.add(this->menu);
-	this->menu.show();
+	Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file(GLADE_PATH + "client_ServerMenu.glade");
+
+	builder->get_widget("error", this->error);
+	builder->get_widget("host", this->host);
+	builder->get_widget("service", this->service);
+	builder->get_widget("connect", this->connect);
+
+	Gtk::Box* menu;
+	builder->get_widget("server_menu", menu);
+	this->window.add(*menu);
+	menu->show();
+
+	this->connect->signal_clicked().connect(sigc::mem_fun(*this, &ServerMenu::connect_button_pressed));
 }
 
 ServerMenu::~ServerMenu(){}
 
 void ServerMenu::connect_button_pressed(){
+	std::string host(this->host->get_text());
+	if (host.empty()){
+		this->error->set_label("Debe ingresar un host");
+		return;
+	}
 
+	std::string service(this->service->get_text());
+	if (service.empty()){
+		this->error->set_label("Debe ingresar un servicio");
+		return;
+	}
+
+	this->connect_to_server(host, service);
+}
+
+void ServerMenu::connect_to_server(const std::string& host, const std::string& service){
+	try{
+		Socket socket(Socket::Client(host.c_str(), service.c_str()));
+		ClientProtocol protocol(std::move(socket));
+	} catch (const SocketException& e){
+		this->error->set_label("No pudo conectarse al servidor");
+	}
 }
--- src/Client/Menu/ServerMenu.h
+++ src/Client/Menu/ServerMenu.h
@@ -7,17 +7,18 @@
 #include <gtkmm/label.h>
 #include <gtkmm/window.h>
 #include "ClientProtocol.h"
+#include <string>
 
 class ServerMenu{
 	private:
-		Gtk::Label error;
-		Gtk::HBox menu;
-		Gtk::Entry host;
-		Gtk::Entry service;
-		Gtk::Button connect;
+		Gtk::Label* error;
+		Gtk::Entry* host;
+		Gtk::Entry* service;
+		Gtk::Button* connect;
 		Gtk::Window& window;
 
 		void connect_button_pressed();
+		void connect_to_server(const std::string& host, const std::string& service);
 
 	public:
 		ServerMenu(Gtk::Window& window);
