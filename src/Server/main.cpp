3dcf3dff1e4eb6f0f0ab98c348c7c6710f28e42e
feldmatias <feldmatias@gmail.com>
2018-05-28 18:58:27 -0300

    refactor main server

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -1,23 +1,18 @@
 #include "Server.h"
 #include <iostream>
 #include <memory>
-#include "Thread.h"
 
 #define EXIT_CHAR 'q'
 
 int main(int argc, const char* argv[]){
-	std::unique_ptr<Thread> thread;
 	try{
-		thread = std::unique_ptr<Thread>(new Server("7777"));
-		thread->start();
+		Server server("7777");
+		server.start();
 		while (std::cin.get() != EXIT_CHAR){}
+		server.stop();
+		server.join();
 	} catch(const std::exception& e){
 		std::cerr << e.what() << std::endl;
-	}
-
-	if (thread){
-		thread->stop();
-		thread->join();
 	}
 	return 0;
 }
