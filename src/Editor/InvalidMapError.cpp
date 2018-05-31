78ba25e43744eab54f415de8a618628801dfac52
Alejandro Daneri <ale_dan_94@hotmail.com>
2018-05-30 21:59:26 -0300

    arreglo error en dialogo de mapa invalido

--- src/Editor/InvalidMapError.cpp
+++ src/Editor/InvalidMapError.cpp
@@ -7,7 +7,9 @@ InvalidMapError::InvalidMapError(const char *message) noexcept : message(message
 }
 
 const char *InvalidMapError::what() const noexcept{
-    Gtk::MessageDialog dialog("Error al guardar archivo",false,Gtk::MESSAGE_WARNING);
+    Gtk::Window dialog_window;
+    Gtk::MessageDialog dialog("Error al guardar archivo", false,Gtk::MESSAGE_WARNING);
+    dialog.set_transient_for(dialog_window);
     dialog.set_secondary_text(message);
     dialog.run();
     return message;
