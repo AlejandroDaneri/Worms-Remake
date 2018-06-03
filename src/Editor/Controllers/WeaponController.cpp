d607467756f76128ff7a756d0d5e4edcffc3000e
Alejandro Daneri <ale_dan_94@hotmail.com>
2018-06-02 21:41:58 -0300

    cambios minimos

--- src/Editor/Controllers/WeaponController.cpp
+++ src/Editor/Controllers/WeaponController.cpp
@@ -5,7 +5,7 @@ WeaponController::WeaponController(std::shared_ptr<WeaponView> View,
                                    std::shared_ptr<Weapon> model)
         : weapon_view(std::move(View)),
           weapon_model(std::move(model)) {
-    weapon_view->linkController(this);
+    weapon_view->bindController(this);
 }
 
 void WeaponController::resetAmmo() {
--- src/Editor/Model/Map.cpp
+++ src/Editor/Model/Map.cpp
@@ -3,38 +3,38 @@
 #include "Map.h"
 
 void Map::erase(int index) {
-    if (!objects.empty())
-        this->objects.erase(objects.begin() + index);
+    if (!contained_objects.empty())
+        this->contained_objects.erase(contained_objects.begin() + index);
 }
 
 void Map::clean() {
-    this->objects.clear();
+    this->contained_objects.clear();
 }
 
 void
 Map::add(unsigned int id, const double &x, const double &y, const int &angle) {
     MapObject new_object(x, y, angle);
-    objects.emplace_back(std::make_pair(id, new_object));
+    contained_objects.emplace_back(std::make_pair(id, new_object));
 }
 
 void Map::move(const int &index, const double &x,const double &y) {
-    MapObject &object = objects[index].second;
+    MapObject &object = contained_objects[index].second;
     object.updatePosition(x, y);
 }
 
 const int Map::turn(const unsigned int &index, unsigned int &id, const int &rotation) {
-    MapObject &object = objects[index].second;
-    id = objects[index].first;
+    MapObject &object = contained_objects[index].second;
+    id = contained_objects[index].first;
     return object.turn(rotation);
 }
 
 const bool Map::isGirder(int &index) const {
-    return (objects[index].first > 1);
+    return (contained_objects[index].first > 1);
 }
 
 void Map::getObjects(std::vector<std::vector<double>> &worms,
                      std::vector<std::vector<double>> &girders) const {
-    for (auto &object : objects) {
+    for (auto &object : contained_objects) {
         float x, y;
         object.second.getPosition(x, y);
         if (object.first == 1) {
@@ -54,7 +54,7 @@ void Map::getObjects(std::vector<std::vector<double>> &worms,
 }
 
 const int Map::getItemID(const int &index) const{
-    return objects[index].first ;
+    return contained_objects[index].first ;
 }
 
 
