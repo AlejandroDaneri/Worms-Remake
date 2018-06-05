567a943d7c00d896a818681b99a47ddac3bc20bc
Alejandro Daneri <ale_dan_94@hotmail.com>
2018-06-05 13:05:49 -0300

    agrego mapas

--- src/CommonUI/ScrollHandler.cpp
+++ src/CommonUI/ScrollHandler.cpp
@@ -2,8 +2,8 @@
 #include <gtkmm/adjustment.h>
 #include <glibmm/main.h>
 
-#define SPACE_TO_SCROLL 30
-#define SCROLL_INCREMENT 50
+#define SPACE_TO_SCROLL 7
+#define SCROLL_INCREMENT 25
 
 ScrollHandler::ScrollHandler(Gtk::ScrolledWindow& window): window(window), last_mouse_position(SPACE_TO_SCROLL * 2, SPACE_TO_SCROLL * 2), mouse_in_window(false){
 	this->window.add_events(Gdk::POINTER_MOTION_MASK);
