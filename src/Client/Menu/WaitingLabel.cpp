837703537883392e3f280017f8a259d9896d12bf
feldmatias <feldmatias@gmail.com>
2018-06-20 18:37:35 -0300

    esperando jugadores imagen

--- src/Client/Menu/WaitingLabel.cpp
+++ src/Client/Menu/WaitingLabel.cpp
@@ -1,17 +1,24 @@
 #include "WaitingLabel.h"
+#include "Path.h"
 #include <string>
 
 const std::string begining("<span size='20000'>");
 const std::string ending("</span>");
 
-WaitingLabel::WaitingLabel() {
+#define SPACING 30
+
+WaitingLabel::WaitingLabel(): container(true, SPACING){
 	this->label.set_use_markup(true);
 	this->label.set_markup(begining + "Esperando jugadores..." + ending);
-	this->label.show();
+	this->image.set(BACKGROUND_MENU_IMAGE);
+	this->container.pack_start(this->image, Gtk::PACK_SHRINK);
+	this->container.pack_start(this->label, Gtk::PACK_SHRINK);
+	this->container.set_halign(Gtk::ALIGN_CENTER);
+	this->container.set_valign(Gtk::ALIGN_CENTER);
 }
 
 WaitingLabel::~WaitingLabel() {}
 
 Gtk::Widget& WaitingLabel::getWidget() {
-	return this->label;
+	return this->container;
 }
--- src/Client/Menu/WaitingLabel.h
+++ src/Client/Menu/WaitingLabel.h
@@ -2,11 +2,15 @@
 #define __WAITINGLABEL_H__
 
 #include <gtkmm/label.h>
+#include <gtkmm/image.h>
+#include <gtkmm/hvbox.h>
 
 /* Label de que indica la espera a otros jugadores */
 class WaitingLabel {
 private:
+	Gtk::VBox container;
 	Gtk::Label label;
+	Gtk::Image image;
 
 public:
 	/* Constructor */
